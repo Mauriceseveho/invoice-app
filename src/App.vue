c422a6039294539ab6f92fbbf504702a00ee436a
yangamaurice <mauriceyanga97@gmail.com>
2025-04-24 11:08:34 +0100

    invoice-app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+
+
+
+      <nav>
+        <RouterLink to="/"></RouterLink>
+      </nav>
+
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="font-bold"> Home</main>
+</template>
